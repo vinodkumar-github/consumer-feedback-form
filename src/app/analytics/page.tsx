736200e8673966eae42ce5c7e6efce1d1dbf5bc3
Vinod Kumar <viki@vinods-macbook-air.local>
2025-07-26 12:24:31 +0530

    Initial commit

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -17,8 +17,8 @@ import {
   ResponseTrendChart,
   QuestionTypeDoughnut,
   prepareChartData
-} from '../../components/analytics/Charts';
-import { BusinessInsights } from '../../components/analytics/BusinessInsights';
+} from '@/components/analytics/Charts';
+import { BusinessInsights } from '@/components/analytics/BusinessInsights';
 import {
   analyzeUserResponses,
   generateProductInsights,
@@ -58,7 +58,7 @@ export default function AnalyticsPage() {
   };
   useEffect(() => {
     checkAuth();
-  }, [checkAuth]);
+  }, []);
 
 
 
